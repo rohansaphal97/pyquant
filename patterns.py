45ff2d1243ab33dadd44230971d8e20a588d7485
Brennan Matriano <brennanmato@gmail.com>
2018-01-29 14:12:11 +0800

    Version 1.0

--- patterns.py
+++ patterns.py
@@ -5,8 +5,13 @@
 # 0 if static
 # Example:
 #   [1,1,1,-1,-1,-1]
-#   Peak
+#     /\
+#    /  \
+#   /    \
+
+# Under construction
 def pattern_search(data,max_period,min_period,format):
     for x in range(min_period,max_period):
-        for y in range(min_period,len(data)):
+        for y in range(0,len(data)):
+            return None
 
