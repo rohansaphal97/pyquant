2ba25b972ce0b40a854601a97cccb5ae07678616
Brennan Matriano <brennanmato@gmail.com>
2018-01-30 18:09:53 +0800

    Added functions to handle data, began matplotlib integration and progress in new engine

--- gui.py
+++ gui.py
@@ -4,21 +4,27 @@ import trading
 import utilities
 import signals
 import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backend_bases import key_press_handler
 
 # class window(Frame):
+#     trading_data =  None
 #     def __init__(self,master=None):
 #         Frame.__init__(self,master)
 #         self.createViews()
 #     def createViews(self):
 #         upload_csv_button = Button(text = "Open CSV", command = self.on_upload_csv_button_click())
 #         upload_csv_button.pack()
+#         stock_price_graph = FigureCanvasTkAgg()
 #     def pass_file(self):
 #         return filedialog.askopenfile(mode="r")
 #     def on_upload_csv_button_click(self):
 #         csv = self.pass_file()
-#         utilities.parse_csv(csv.name)
+#         self.trading_data = utilities.parse_csv(csv.name)
 # def main():
 #     root = Tk()
+#     root.wm_title("QTS Trading Simulator")
 #     root.geometry("250x150+300+300")
 #     app = window()
 #     root.mainloop()
